43e6bcda6ef2aa9714cff315e3967e0be6f04cb7
Richard Mao <richard.mao.123@gmail.com>
2016-03-06 11:41:12 -0800

    HighScore
    Added a highscore counter to keep track of the user’s local highscore.
    Also formatted the end scene

--- EndScene.swift
+++ EndScene.swift
@@ -8,29 +8,33 @@
 
 import Foundation
 import SpriteKit
+import GameKit
+
 
 class EndScene : SKScene //super class is SKScene
 {
     var restartBtn : UIButton! //instantiates this var as a UI button
-    var highScore : Int!
     var ScoreLbl : UILabel!
     var GameOverLbl : UILabel!
+    var HighScoreLbl: UILabel!
     
-    var blah = false;
+    var didRestart = false;
     override func didMoveToView(view: SKView)
     {
-        scene?.backgroundColor = UIColor.blackColor()
+        scene?.backgroundColor = UIColor.darkGrayColor()
         
         
         
-        GameOverLbl = UILabel(frame: CGRect(x: 0, y: 0, width: view.frame.size.width / 3, height: 30))
+        GameOverLbl = UILabel(frame: CGRect(x: 0, y: 0, width: view.frame.size.width / 1.5, height: 120))
         GameOverLbl.center = CGPoint(x: view.frame.size.width / 2 + 20, y: view.frame.size.width / 7)
+        GameOverLbl.font = GameOverLbl.font.fontWithSize(50)
         GameOverLbl.text = "GAME OVER"
         GameOverLbl.textColor = UIColor.redColor()
         self.view?.addSubview(GameOverLbl)
         
-        restartBtn = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width / 3, height: 60))
-        restartBtn.center = CGPoint(x: view.frame.size.width / 2, y: view.frame.size.width / 5)
+        restartBtn = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width / 3, height: 120))
+        
+        restartBtn.center = CGPoint(x: view.frame.size.width / 2, y: view.frame.size.width / 4)
         restartBtn.setTitle("Restart?", forState: UIControlState.Normal) //text says "restart" when nothing is pressed
         restartBtn.setTitleColor(UIColor.redColor(), forState: UIControlState.Normal)
         restartBtn.addTarget(self, action: Selector("Restart"), forControlEvents: UIControlEvents.TouchUpInside) //once the button is released, call a function Restart()
@@ -42,27 +46,38 @@ class EndScene : SKScene //super class is SKScene
         
         ScoreLbl = UILabel(frame: CGRect(x: 0, y:0, width: view.frame.size.width / 3, height: 30))
         
-        ScoreLbl.center = CGPoint(x: view.frame.size.width / 2 + 35, y: view.frame.size.width / 4)
+        ScoreLbl.center = CGPoint(x: view.frame.size.width / 2 + 35, y: view.frame.size.width / 3)
         ScoreLbl.text = "Score: \(score)"
         ScoreLbl.textColor = UIColor.whiteColor()
         self.view?.addSubview(ScoreLbl)
         
-/*        var highscoreDefault = NSUserDefaults.standardUserDefaults()
-        highscoreDefault.setValue(score, forKey: "HighScore")
-        highScore = highscoreDefault.valueForKey("HighScore") as! NSInteger
-        NSLog("\(highScore)")
-  */      
+       var highScoreDefault = NSUserDefaults.standardUserDefaults()
+        var highScore = highScoreDefault.valueForKey("HighScore") as! NSInteger
+        
+        HighScoreLbl = UILabel(frame: CGRect(x: 0, y: 0, width: view.frame.size.width/3, height: 30))
+        HighScoreLbl.font = HighScoreLbl.font.fontWithSize(10)
+        HighScoreLbl.center = CGPoint(x: 35 + view.frame.size.width / 2, y: view.frame.size.width / 3 + 25)
+        
+        
+        
+        
+        HighScoreLbl.text = "HighScore: \(highScore)"
+        HighScoreLbl.textColor = UIColor.whiteColor()
+        self.view?.addSubview(HighScoreLbl)
+        
     }
     
     func Restart()
     {
-        if(blah == false) //incase restart is called multiple times during updates
+        if(didRestart == false) //incase restart is called multiple times during updates
         {
             self.view?.presentScene(GameScene(), transition: SKTransition.crossFadeWithDuration(0.3)) //TODO: HUGE ISSUE WITH PRESENTING SCENE
             restartBtn.removeFromSuperview()
             ScoreLbl.removeFromSuperview()
             GameOverLbl.removeFromSuperview()
-            blah = true;
+            HighScoreLbl.removeFromSuperview()
+            ScoreLbl.removeFromSuperview()
+            didRestart = true;
         }
     }
 }
